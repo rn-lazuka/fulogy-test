16c5fd02beee883440cf4741962ed71d0d7b5dc5
rn-lazuka <rn.lazuka@gmail.com>
2020-07-06 01:52:32 +0300

    Updates

---
+++ precache-manifest.f57999861697895df6a770ec4e58a4bd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1e32e83adb6ec9f970cc4a11fcee0e",
+    "url": "/fulogy-test/index.html"
+  },
+  {
+    "revision": "88d5dec30c3ca02593bc",
+    "url": "/fulogy-test/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "21b0a60a3ddef4734873",
+    "url": "/fulogy-test/static/css/main.52f82c9e.chunk.css"
+  },
+  {
+    "revision": "88d5dec30c3ca02593bc",
+    "url": "/fulogy-test/static/js/2.d162d1b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fulogy-test/static/js/2.d162d1b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b0a60a3ddef4734873",
+    "url": "/fulogy-test/static/js/main.b7d56d0f.chunk.js"
+  },
+  {
+    "revision": "f11ba13ff17d8fb90d2d",
+    "url": "/fulogy-test/static/js/runtime-main.d994e451.js"
+  },
+  {
+    "revision": "1b49837c1c8b08d8d26d7fe20f78e9fb",
+    "url": "/fulogy-test/static/media/coldlight.1b49837c.png"
+  },
+  {
+    "revision": "1b49837c1c8b08d8d26d7fe20f78e9fb",
+    "url": "/fulogy-test/static/media/daylight.1b49837c.png"
+  },
+  {
+    "revision": "1c48f0063c211cdf5bf8421462b56168",
+    "url": "/fulogy-test/static/media/light-photo.1c48f006.png"
+  },
+  {
+    "revision": "1c48f0063c211cdf5bf8421462b56168",
+    "url": "/fulogy-test/static/media/light-photo2.1c48f006.png"
+  },
+  {
+    "revision": "1c48f0063c211cdf5bf8421462b56168",
+    "url": "/fulogy-test/static/media/light-photo3.1c48f006.png"
+  },
+  {
+    "revision": "1b49837c1c8b08d8d26d7fe20f78e9fb",
+    "url": "/fulogy-test/static/media/warmlight.1b49837c.png"
+  }
+]);
